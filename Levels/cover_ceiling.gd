a6961158e5552e1e8799b5c2c9e1f77768749652
ioshY-k <joshyk1307@gmail.com>
2025-12-18 19:43:23 +0100

    Add Cover Ceiling Objects

---
+++ Levels/cover_ceiling.gd
@@ -0,0 +1,23 @@
+@tool
+extends Sprite2D
+
+@export var sprite:Texture2D:
+	set(value):
+		sprite = value
+		texture = value
+
+@onready var area_2d: Area2D = $Area2D
+@onready var collision_polygon_2d: CollisionPolygon2D = $Area2D/CollisionPolygon2D
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("in")
+	var tween:Tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(self, "modulate:a", 0.1, 0.4)
+
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	print("out")
+	var tween:Tween = get_tree().create_tween()
+	tween.tween_property(self, "modulate:a", 1, 0.4).set_trans(Tween.TRANS_LINEAR)
