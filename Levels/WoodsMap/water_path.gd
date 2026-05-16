4d79cb9317c6724bc17c9fafa32d0e27e080f79a
ioshY-k <joshyk1307@gmail.com>
2026-05-16 21:07:05 +0200

    update map previews

--- Levels/WoodsMap/water_path.gd
+++ Levels/WoodsMap/water_path.gd
@@ -10,7 +10,7 @@ func spawn_waterdrop():
 	var drop: PathFollow2D = WATER_DROP_PATH_FOLLOW.instantiate()
 	add_child(drop)
 	var tween = get_tree().create_tween()
-	tween.tween_property(drop, "progress_ratio", 1, randf_range(2.3,2.9))
+	tween.tween_property(drop, "progress_ratio", 1, randf_range(3.0,3.8))
 	timer.wait_time = randf_range(0.4,1.4)
 	await tween.finished
 	drop.queue_free()
--- Levels/WoodsMap/waterdrop.gd
+++ Levels/WoodsMap/waterdrop.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 var opacity = 0.0
+var jitterplace = Vector2(randf_range(0.0,10.0),randf_range(0.0,10.0))
 
 func _ready() -> void:
 	self_modulate.a = randf_range(0.6,0.9)
@@ -9,8 +10,12 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	modulate.a = lerp(modulate.a , opacity, randf_range(0.05,0.25))
+	position.x = lerp(position.x , jitterplace.x, randf_range(0.3,1.0))
+	position.y = lerp(position.y , jitterplace.y, randf_range(0.3,1.0))
 	if modulate.a < opacity + 0.01 and modulate.a > opacity - 0.01:
 		if opacity == 0.0:
 			opacity = randf_range(0.4,0.8)
 		else:
 			opacity = 0.0
+	if position.x < jitterplace.x + 0.1 and position.x > jitterplace.x - 0.1:
+		jitterplace = Vector2(randf_range(0.0,10.0),randf_range(0.0,10.0))
